734ff1f2a2efb0fa99b951413ca1edebf2b5447e
Alexey Peskov <aspeskov@gmail.com>
2016-11-11 20:06:08 +0100

    Tests are classes again to be picked up by Travis

--- src/test/kotlin/com/sandlex/trykotlin/BinarySearchTreeTest.kt
+++ src/test/kotlin/com/sandlex/trykotlin/BinarySearchTreeTest.kt
@@ -9,35 +9,37 @@ import org.junit.Test
  *
  * Write a function that checks if a given binary search tree contains a given value.
  */
-@Test
-fun testBstContainsShort() {
-    val n1 = Node(1, null, null)
-    val n3 = Node(3, null, null)
-    val n2 = Node(2, n1, n3)
+class BinarySearchTreeTest {
+    @Test
+    fun testBstContainsShort() {
+        val n1 = Node(1, null, null)
+        val n3 = Node(3, null, null)
+        val n2 = Node(2, n1, n3)
 
-    Assert.assertTrue(bstContains(n2, 3))
-    Assert.assertTrue(bstContains(n2, 1))
-    Assert.assertTrue(bstContains(n1, 1))
-    Assert.assertFalse(bstContains(n2, 7))
-}
+        Assert.assertTrue(bstContains(n2, 3))
+        Assert.assertTrue(bstContains(n2, 1))
+        Assert.assertTrue(bstContains(n1, 1))
+        Assert.assertFalse(bstContains(n2, 7))
+    }
 
-@Test
-fun testBstContainsLong() {
-    val n2 = Node(2, null, null)
-    val n6 = Node(6, null, null)
-    val n8 = Node(8, null, null)
-    val n10 = Node(10, null, null)
-    val n12 = Node(12, null, null)
-    val n14 = Node(14, null, null)
-    val n20 = Node(20, null, null)
-    val n4 = Node(4, n2, n6)
-    val n7 = Node(7, n4, n8)
-    val n11 = Node(11, n10, n12)
-    val n9 = Node(9, n7, n11)
-    val n16 = Node(16, n14, n20)
-    val n13 = Node(13, n9, n16)
+    @Test
+    fun testBstContainsLong() {
+        val n2 = Node(2, null, null)
+        val n6 = Node(6, null, null)
+        val n8 = Node(8, null, null)
+        val n10 = Node(10, null, null)
+        val n12 = Node(12, null, null)
+        val n14 = Node(14, null, null)
+        val n20 = Node(20, null, null)
+        val n4 = Node(4, n2, n6)
+        val n7 = Node(7, n4, n8)
+        val n11 = Node(11, n10, n12)
+        val n9 = Node(9, n7, n11)
+        val n16 = Node(16, n14, n20)
+        val n13 = Node(13, n9, n16)
 
-    Assert.assertTrue(bstContains(n13, 11))
-    Assert.assertTrue(bstContains(n9, 12))
-    Assert.assertFalse(bstContains(n7, 9))
+        Assert.assertTrue(bstContains(n13, 11))
+        Assert.assertTrue(bstContains(n9, 12))
+        Assert.assertFalse(bstContains(n7, 9))
+    }
 }
--- src/test/kotlin/com/sandlex/trykotlin/SortedSearchTest.kt
+++ src/test/kotlin/com/sandlex/trykotlin/SortedSearchTest.kt
@@ -7,9 +7,11 @@ import org.junit.Test
  * Implement function countNumbers that accepts a sorted array of integers and counts the number of array elements
  * that are less than the parameter lessThan.
  */
-@Test
-fun testCountNumbers() {
-    Assert.assertEquals(2, countNumbers(intArrayOf(1, 3, 5, 7), 4))
-    Assert.assertEquals(4, countNumbers(intArrayOf(1, 3, 5, 7), 8))
-    Assert.assertEquals(0, countNumbers(intArrayOf(1, 3, 5, 7), 0))
+class SortedSearchTest {
+    @Test
+    fun testCountNumbers() {
+        Assert.assertEquals(2, countNumbers(intArrayOf(1, 3, 5, 7), 4))
+        Assert.assertEquals(4, countNumbers(intArrayOf(1, 3, 5, 7), 8))
+        Assert.assertEquals(0, countNumbers(intArrayOf(1, 3, 5, 7), 0))
+    }
 }
